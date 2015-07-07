6897ba8dd2723133be95d049721a820419fe6583
Tom Beigle <tom@designated-developers.com>
2015-07-07 13:56:40 -0500

    TNS-6: Added mobile menu functionality.

--- docroot/sites/all/themes/tns/assets/js/tns.js
+++ docroot/sites/all/themes/tns/assets/js/tns.js
@@ -1,43 +1,43 @@
 (function($) {
   Drupal.behaviors.tns = {
     attach: function(context, settings) {
-      var socialMedia = $("ul.social-media");
-      var footer = $("div.region-footer");
-      var footerPosition = socialMedia.offset().top + socialMedia.outerHeight() - $(window).scrollTop();
-      var footerHeight = footer.height();
+      var $socialMedia = $("ul.social-media");
+      var $footer = $("div.region-footer");
+      var footerPosition = $socialMedia.offset().top + $socialMedia.outerHeight() - $(window).scrollTop();
+      var footerHeight = $footer.height();
       var windowHeight = $(window).height();
       var footerFixed = false;
       var headerHeight = $("div#headerwrap").height();
-      var headerMenu = $("div#block-system-main-menu");
+      var $navWrap = $('#primary-navigation');
+      var $headerMenu = $("#block-system-main-menu");
       var menuMargin;
-      var headerLogo = $("#logo");
+      var $headerLogo = $("#logo");
 
       function positionFooter() {
-        footerPosition = socialMedia.offset().top + socialMedia.outerHeight() - $(window).scrollTop();
+        footerPosition = $socialMedia.offset().top + $socialMedia.outerHeight() - $(window).scrollTop();
         
         if ((footerPosition + footerHeight > windowHeight) && (footerFixed == false)) {
-          footer.css({position: "fixed", bottom: "0"});
+          $footer.css({position: "fixed", bottom: "0"});
           footerFixed = true;
-          socialMedia.css({marginBottom: footerHeight});
+          $socialMedia.css({marginBottom: footerHeight});
         }
         else if ((footerPosition + footerHeight < windowHeight) && (footerFixed == true)) {
-          footer.css({position: "static", bottom: ""});
+          $footer.css({position: "static", bottom: ""});
           footerFixed = false;
-          socialMedia.css({marginBottom: 0});
+          $socialMedia.css({marginBottom: 0});
         }
       }
       
       function positionHeaderMenu() {
-        //alert($(window).width() + " " + headerLogo.width() + " " + headerMenu.width());
-	      if ($(window).width() < headerLogo.outerWidth(true) + headerMenu.outerWidth(true)) {
-	        headerMenu.css({
+	      if ($(window).width() < $headerLogo.outerWidth(true) + $headerMenu.outerWidth(true)) {
+	        $headerMenu.css({
   	        marginTop: "10px",
   	        float: "left"  	        
   	      });
 	      }
 	      else {
-	        headerMenu.css({
-  	        marginTop: headerHeight - headerMenu.height(),
+	        $headerMenu.css({
+  	        marginTop: headerHeight - $headerMenu.height(),
   	        float: "right"
   	      });
 	      }
@@ -52,9 +52,19 @@
       });
       $(window).resize(function() {
         windowHeight = $(window).height();
-        footerHeight = footer.height();
+        footerHeight = $footer.height();
         positionFooter();
         positionHeaderMenu();
+      });
+      
+      // Add the mobile menu link to the main menu
+      $navWrap.prepend('<a href="#block-system-main-menu" class="mobile-menu-icon" title="Click/tap to open the main menu">i</a>');
+      $('.mobile-menu-icon').click(function(e) {
+        e.preventDefault();
+        
+        var $target = $($(this).attr('href'));
+        $target.slideToggle('fast');
+        $(this).toggleClass('open');
       });
     }
   }
